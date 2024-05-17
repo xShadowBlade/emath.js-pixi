19fbad600917dbf83586cc23e46d87e6dc8ec577
zBlade <dnguy3n2010@gmail.com>
2024-05-16 20:05:26 -0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @file Main entry point for the pixiGame package.
+ */
+import { hookPixiGame } from "./hookPixiGame";
+hookPixiGame();
+
+export * from "./PixiGame";
+export * from "./Sprite";
